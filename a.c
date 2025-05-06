dabc2049f60e0e2b1d4a08a0293e3efb091e6195
ashmitha1234 <ashmitha.s@s.amity.edu>
2025-05-06 11:24:22 +0530

    Creating a program to check if a given number is a armstrong number

--- a.c
+++ a.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
 int main{
+	int num, copy, sum=0;
+    printf("Enter a number : ");
+    scanf("%d", &num); //153
+    copy = num;
+    while(num!=0){
+    int d = num % 10; //3 5 1
+    sum += d * d * d; //9 152 153
+    num = num / 10; //15 1 0
+    }
 }
 
